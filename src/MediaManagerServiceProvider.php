00d3ce599912db812f2e289b95151209d7927d25
Nuwan <nuwanchamp02@gmail.com>
2022-01-27 15:06:40 +0530

    Package skelaton with Spatied Media Library requirements

---
+++ src/MediaManagerServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sentech\MediaManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class MediaManagerServiceProvider extends ServiceProvider
+{
+    public function boot(){
+
+    }
+    public function register()
+    {
+        parent::register();
+    }
+
+}
